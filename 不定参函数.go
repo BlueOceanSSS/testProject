ef97fafdee8e53c8754389ee6eabd6aeda149449
duanshanhu <duanshanhu@bytedance.com>
2021-04-21 09:05:38 +0800

    修改格式

--- 不定参函数.go
+++ 不定参函数.go
@@ -4,17 +4,16 @@ import "fmt"
 
 func main() {
 	result := myfunc()
-	fmt.Println("result = ",result)
+	fmt.Println("result = ", result)
 }
-func myfunc (args ...int) (result int){
+func myfunc(args ...int) (result int) {
 	//建议使用(result int)方式     args可以传多个，也可以传0个
-	for _,data:=  range args{
+	for _, data := range args {
 		//i是args的下标，data是args的元素
 		fmt.Println(data)
-		result+=data
-	//	等价于result = result+data
+		result += data
+		//	等价于result = result+data
 	}
 	return
-//	默认返回result
+	//	默认返回result
 }
-
